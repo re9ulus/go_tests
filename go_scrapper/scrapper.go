2962f091ebc8608f179163bb08af596ef874daa0
Vasiliy Zemlyanov <angeldaren@gmail.com>
2016-06-09 01:08:40 +0300

    Update scrapper, add command line args

--- go_scrapper/scrapper.go
+++ go_scrapper/scrapper.go
@@ -1,15 +1,9 @@
 // by re9ulus 30.05.2016
 
-/*
-TODO:
-1. Pass url as command arg
-2. Save '.html' pages to disc
-3. Pass path to 'save' folder as command arg
-*/
-
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io/ioutil"
@@ -36,23 +30,29 @@ func savePage(filename, html string) {
 
 func urlToFilename(url string) string {
 	filename := strings.Replace(url, "http://", "", -1)
+	filename = strings.Replace(filename, "https://", "", -1)
 	filename = strings.Replace(filename, "/", "_", -1)
-	// filename = strings.Replace(filename, ":", "", -1)
 	return filename
 }
 
-func getUrlsFromPage(url string, newLinkChan chan string) {
+func getUrlsFromPage(url string, newLinkChan chan string, saveFolder string) {
 	fmt.Println("Url to process: ", url)
 	doc, err := goquery.NewDocument(url)
-	checkError(err)
+	if err != nil {
+		fmt.Println("error occured", "error: %s", err.Error())
+		return
+	}
 
 	// fmt.Println(doc.Html())
-	if strings.HasSuffix(url, ".html") {
-		content, err := doc.Html()
-		checkError(err)
-
-		go savePage("F:/test/"+urlToFilename(url), content)
+	// if strings.HasSuffix(url, ".html") {
+	content, err := doc.Html()
+	if err != nil {
+		fmt.Println("error occured", "error: %s", err.Error())
+		return
 	}
+
+	go savePage(saveFolder+urlToFilename(url), content)
+	// }
 
 	checkError(err)
 	doc.Find("a").Each(func(i int, s *goquery.Selection) {
@@ -64,13 +64,16 @@ func getUrlsFromPage(url string, newLinkChan chan string) {
 }
 
 func main() {
-	urlRoot := "http://www.tornadoweb.org/en/stable/"
+	var urlRoot, saveFolder string
+	flag.StringVar(&urlRoot, "root", "", "page root")
+	flag.StringVar(&saveFolder, "save_folder", "F:/test/", "folder to save pages")
+	flag.Parse()
 	visitedLinks := make(map[string]bool)
 	visitedLinks[""] = true
 	defaultUrl := ""
 	newLinkChan := make(chan string)
 
-	go getUrlsFromPage(urlRoot+defaultUrl, newLinkChan)
+	go getUrlsFromPage(urlRoot+defaultUrl, newLinkChan, saveFolder)
 
 	stopCounter := 0
 	for stopCounter < 3 {
@@ -80,7 +83,7 @@ func main() {
 			_, ok := visitedLinks[newLink]
 			if !ok {
 				visitedLinks[newLink] = true
-				go getUrlsFromPage(urlRoot+newLink, newLinkChan)
+				go getUrlsFromPage(urlRoot+newLink, newLinkChan, saveFolder)
 			}
 		case <-time.After(time.Second):
 			fmt.Println("exit?")
@@ -91,7 +94,7 @@ func main() {
 	for key, _ := range visitedLinks {
 		fmt.Println(key)
 	}
-	fmt.Println(len(visitedLinks))
 
+	fmt.Println("Pages found:", len(visitedLinks))
 	fmt.Println("Exit from main")
 }
